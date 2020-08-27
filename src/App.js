d373af1213d692d3711e6352ff0e7a404512888c
Krzysztof Wicki <krzysztof_wicki@sevenet.pl>
2020-08-27 20:45:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PageWrapper from './components/PageWrapper';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Pages
+import Home from './components/Pages/Home';
+import Currency from './components/Pages/Currency';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <PageWrapper>
+              <Route
+                exact={true}
+                path = "/"
+                component = {Home}
+              />
+              <Route
+                path = "/currency"
+                component = {Currency}
+              />
+
+          </PageWrapper>
+        </Router>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Header from '../Common/Header';
+import image from '../assets/img/header-bg.jpg';
+
+class Home extends Component {
+    
+    render(){
+        return (
+            <div>
+                <Header
+                    title = "strona służąca do sprawdzania kursów walut"
+                    subtitle = "Witaj"
+                    buttonText = "Waluty"
+                    link="/services"
+                    showButton={true}
+                    image={image}
+
+                />
+            </div>
+        )
+    }
+}
+
+export default Home;
